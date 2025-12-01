a5469e29d5719abcbf2416ebd5d4a09e40d5fd5b
Ricardo Nardi <ricardonardirocha@gmail.com>
2025-12-01 18:58:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2526372464-fbfa9",
+  "appId": "1:2499568314:web:00ba4154a87518b0011883",
+  "apiKey": "AIzaSyDEFxCI4xvemeIG00XyvHQz1abQV1Mit6E",
+  "authDomain": "studio-2526372464-fbfa9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "2499568314"
+};
