0b674c2522342032783ae3f85c85e569af5da765
Ricardo Nardi <ricardonardirocha@gmail.com>
2025-12-04 15:25:07 +0000

    ja tenho sim, provavelmente estou no lugar errado no shell
    https://con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "contabilx-77490593-12929",
-  "appId": "1:2499568314:web:00ba4154a87518b0011883",
-  "apiKey": "AIzaSyDEFxCI4xvemeIG00XyvHQz1abQV1Mit6E",
-  "authDomain": "contabilx-77490593-12929.firebaseapp.com",
-  "storageBucket": "contabilx-77490593-12929.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "2499568314"
+  "projectId": "studio-2526372464-fbfa9",
+  "appId": "1:521360098592:web:65a1f6b15a7c29668f45a7",
+  "apiKey": "AIzaSyAz_Qd11n5gE4GgR1PcF4aWdCj18HwM3c4",
+  "authDomain": "studio-2526372464-fbfa9.firebaseapp.com",
+  "storageBucket": "studio-2526372464-fbfa9.appspot.com",
+  "measurementId": "G-5G3QYJ2DRV",
+  "messagingSenderId": "521360098592"
 };
