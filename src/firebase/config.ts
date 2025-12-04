406fe814b1823fbc665583312468e97052a6ce62
Ricardo Nardi <ricardonardirocha@gmail.com>
2025-12-04 15:19:11 +0000

    nao funcionou ainda, estou nesse lugar ai

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-2526372464-fbfa9",
+  "projectId": "contabilx-77490593-12929",
   "appId": "1:2499568314:web:00ba4154a87518b0011883",
   "apiKey": "AIzaSyDEFxCI4xvemeIG00XyvHQz1abQV1Mit6E",
-  "authDomain": "studio-2526372464-fbfa9.firebaseapp.com",
-  "storageBucket": "studio-2526372464-fbfa9.appspot.com",
+  "authDomain": "contabilx-77490593-12929.firebaseapp.com",
+  "storageBucket": "contabilx-77490593-12929.appspot.com",
   "measurementId": "",
   "messagingSenderId": "2499568314"
 };
