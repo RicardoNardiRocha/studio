a1cba7f376f64ec574f2cdd6f3ae0e9ae66699a8
contabilx <suporte.contabilx@gmail.com>
2025-12-11 18:24:35 +0000

    Try fixing this error: `Runtime Error: [object Object] was not properly

--- src/components/dashboard/recent-activities.tsx
+++ src/components/dashboard/recent-activities.tsx
@@ -1,11 +1,10 @@
 'use client';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Skeleton } from '@/components/ui/skeleton';
-import { useCollection, useFirestore } from '@/firebase';
+import { useCollection, useFirestore, useMemoFirebase } from '@/firebase';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { collection, limit, orderBy, query, Timestamp } from 'firebase/firestore';
-import { useMemo } from 'react';
 import { User } from 'lucide-react';
 
 interface Activity {
@@ -20,7 +19,7 @@ interface Activity {
 export function RecentActivities() {
   const firestore = useFirestore();
 
-  const activitiesQuery = useMemo(() => {
+  const activitiesQuery = useMemoFirebase(() => {
     if (!firestore) return null;
     return query(collection(firestore, 'activities'), orderBy('timestamp', 'desc'), limit(5));
   }, [firestore]);
