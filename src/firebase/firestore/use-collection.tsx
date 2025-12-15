a096e114cef1bb081ff1b6936917bee02eb6103a
Ricardo Nardi <ricardonardirocha@gmail.com>
2025-12-15 14:33:22 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Missing or insuffi

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -93,10 +93,13 @@ export function useCollection<T = any>(
       },
       (error: FirestoreError) => {
         // This logic extracts the path from either a ref or a query
-        const path: string =
-          memoizedTargetRefOrQuery.type === 'collection'
-            ? (memoizedTargetRefOrQuery as CollectionReference).path
-            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
+        // Ensure the query object exists before trying to access its properties
+        let path: string = 'caminho_desconhecido';
+        if (memoizedTargetRefOrQuery) {
+            path = memoizedTargetRefOrQuery.type === 'collection'
+                ? (memoizedTargetRefOrQuery as CollectionReference).path
+                : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString();
+        }
 
         const contextualError = new FirestorePermissionError({
           operation: 'list',
